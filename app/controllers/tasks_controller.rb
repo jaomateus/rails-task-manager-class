f7c647a4e5aa146c0a3a0e0a75a2a5e0d0d96373
Joao Mateus <jaomateus@gmail.com>
2023-10-23 11:37:03 +0100

    refactor tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,51 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # View all the tasks
   def index
     @tasks = Task.all
   end
 
-  def view
+  # View the details of one task
+  def show
+  end
+
+  # Create a new task
+
+  # Open empty form to fill the task details
+  def new
+    @task = Task.new
+  end
+
+  # create a new task with the data filled on the form
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  # Edit an existing Task
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  #delete an existing task
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
     @task = Task.find(params[:id])
-    raise
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "tasks", to: 'tasks#index'
-  get "tasks/:id", to: 'tasks#view', as: 'task'
+  # get "tasks", to: 'tasks#index'
+  # get "tasks/new", to: 'tasks#new'
+  # get "tasks/:id", to: 'tasks#view', as: :task
+  # post "tasks", to: 'tasks#create'
+  # get "tasks/:id/edit", to: 'tasks#edit', as: :edit_path
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
+
 end
